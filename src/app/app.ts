2c67e1620882b97a5ffa0045eb53dd15baa3e3a6
John Simpson <jsimpson@precisionhealth.net>
2026-05-29 13:50:56 -0400

    Updated to support new embedded workflow component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,26 @@
 import { ChangeDetectionStrategy, Component, inject, input, InputSignal, OnInit, signal, ViewEncapsulation, WritableSignal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { MPageService, MpageLogComponent, AddressService, AllergyService, CodeValueService, ConfigService, CustomService, DiagnosisService, EncounterService, Dialog, OrganizationService, PersonService, PhoneService, ProblemService, ReferenceService, PrsnlService, IMenuItem, TabbedMenuComponent } from '@clinicaloffice/mpage-developer';
+import {
+  MPageService,
+  MpageLogComponent,
+  AddressService,
+  AllergyService,
+  CodeValueService,
+  ConfigService,
+  CustomService,
+  DiagnosisService,
+  EncounterService,
+  Dialog,
+  OrganizationService,
+  PersonService,
+  PhoneService,
+  ProblemService,
+  ReferenceService,
+  PrsnlService,
+  IMenuItem,
+  TabbedMenuComponent,
+  MPageLogService
+} from '@clinicaloffice/mpage-developer';
 import { Common } from './services/common';
 import { Information } from './components/information/information';
 import { Relationships } from './components/relationships/relationships';
@@ -16,7 +36,7 @@ declare const VERSION: string;
   styleUrls: ['../styles.scss', '../clinical-office-styles.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.ShadowDom,
-  providers: [MPageService, AddressService, AllergyService, CodeValueService, ConfigService, CustomService,
+  providers: [MPageService, MPageLogService, AddressService, AllergyService, CodeValueService, ConfigService, CustomService,
     DiagnosisService, EncounterService, Dialog, OrganizationService, PersonService, PhoneService,
     ProblemService, PrsnlService, ReferenceService, Common]
 })
@@ -27,6 +47,10 @@ export class App implements OnInit {
 
   public title: InputSignal<string> = input('default');
   public path: InputSignal<string> = input('path');
+  public person_id: InputSignal<string> = input('');
+  public encntr_id: InputSignal<string> = input('');
+  public prsnl_id: InputSignal<string> = input('');
+  public disable_debugger: InputSignal<string> = input('0');
 
   public menuItems: IMenuItem[] = [
     { label: 'Information', idName: 'info', icon: 'contact_mail' },
@@ -39,9 +63,10 @@ export class App implements OnInit {
   ngOnInit() {
     // Grab any parameters in the URL (Used in Cerner Components)
     this.activatedRoute.queryParams.subscribe(params => {
-      this.MPage.personId = params['personId'] ? parseInt(params['personId']) : this.MPage.personId;
-      this.MPage.encntrId = params['encounterId'] ? parseInt(params['encounterId']) : this.MPage.encntrId;
-      this.MPage.prsnlId = params['userId'] ? parseInt(params['userId']) : this.MPage.prsnlId;
+      this.MPage.personId = params['personId'] ? parseInt(params['personId']) : this.person_id() !== '' ? parseInt(this.person_id()) : this.MPage.personId;
+      this.MPage.encntrId = params['encounterId'] ? parseInt(params['encounterId']) : this.encntr_id() !== '' ? parseInt(this.encntr_id()) : this.MPage.encntrId;
+      this.MPage.prsnlId = params['userId'] ? parseInt(params['userId']) : this.prsnl_id() !== '' ? parseInt(this.prsnl_id()) : this.MPage.prsnlId;
+      this.MPage.disableDebugger = parseInt(this.disable_debugger()) === 1;
     });
 
     this.MPage.setMaxInstances(2, true, 'CHART', false);
